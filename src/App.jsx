da6d52efafdf668b0ebb48b28167c26de7210064
Kritika131 <guptamushkan817@gmail.com>
2023-06-13 12:06:14 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { getApiConfiguration,getGenres } from './store/homeSlice';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-import Footer from './components/footer/Footer';
+import Footer from './components/footer/footer';
 import ErrorPage from './components/404/ErrorPage'
 import Details from './components/details/Details';
 import SearchResult from './components/searchResult/SearchResult'
